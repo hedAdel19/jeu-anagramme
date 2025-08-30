936cda1bbac870800a07d476ccac18ab8383c261
hedAdel19 <120309863+hedAdel19@users.noreply.github.com>
2025-08-30 23:28:04 +0100

    Update Timer.js

--- docs/src/components/Timer.js
+++ docs/src/components/Timer.js
@@ -1,1 +1,24 @@
+class Timer {
+  constructor() {
+    this.interval = null;
+  }
 
+  start(duration, onTick, onEnd) {
+    let timeLeft = duration;
+    onTick(timeLeft);
+
+    this.interval = setInterval(() => {
+      timeLeft--;
+      onTick(timeLeft);
+
+      if (timeLeft <= 0) {
+        clearInterval(this.interval);
+        onEnd();
+      }
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+}
