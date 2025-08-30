daf17b5230a72741a2b9ee05af7d75600f8d34b7
hedAdel19 <120309863+hedAdel19@users.noreply.github.com>
2025-08-30 20:36:48 +0100

    Update Storage.js

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -1,1 +1,18 @@
+class Storage {
+  constructor() {
+    this.key = "anagramme_scores";
+  }
 
+  save(scores) {
+    localStorage.setItem(this.key, JSON.stringify(scores));
+  }
+
+  load() {
+    const data = localStorage.getItem(this.key);
+    return data ? JSON.parse(data) : [];
+  }
+
+  clear() {
+    localStorage.removeItem(this.key);
+  }
+}
