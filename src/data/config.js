6cf7fe80a24b6de901172d04fcd883fe1ff8c347
hedAdel19 <120309863+hedAdel19@users.noreply.github.com>
2025-08-30 20:38:20 +0100

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,1 +1,8 @@
-
+const config = {
+  dureeInitiale: 30, // durée du minuteur en secondes
+  pointsParBonneReponse: 10,
+  niveauParDefaut: "FACILE",
+  nombreMaxScores: 10,
+  langue: "fr",
+  theme: "neon"
+};
