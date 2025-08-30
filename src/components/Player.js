de1ded45c5ca0f65ee6812f33c42e5e90aeaf5fc
hedAdel19 <120309863+hedAdel19@users.noreply.github.com>
2025-08-30 20:32:15 +0100

    Update Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,1 +1,14 @@
+class Player {
+  constructor() {
+    this.name = "Joueur";
+    this.score = 0;
+  }
 
+  incrementScore(points) {
+    this.score += points;
+  }
+
+  resetScore() {
+    this.score = 0;
+  }
+}
