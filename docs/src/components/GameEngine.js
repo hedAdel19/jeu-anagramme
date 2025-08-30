897eb7092e88eb98367fd54a6d40d78420d81623
hedAdel19 <120309863+hedAdel19@users.noreply.github.com>
2025-08-30 23:15:50 +0100

    Update GameEngine.js

--- docs/src/components/GameEngine.js
+++ docs/src/components/GameEngine.js
@@ -1,1 +1,53 @@
+class GameEngine {
+  constructor() {
+    this.words = [];
+    this.definitions = {};
+    this.currentIndex = 0;
+    this.level = "FACILE";
+  }
+
+  loadWords(wordList) {
+    this.words = wordList;
+    this.shuffleWords();
+  }
+
+  loadDefinitions(defs) {
+    this.definitions = defs;
+  }
+
+  shuffleWords() {
+    this.words.sort(() => Math.random() - 0.5);
+  }
+
+  getCurrentWord() {
+    return this.words[this.currentIndex];
+  }
+
+  getCurrentAnagram() {
+    return this.shuffleLetters(this.getCurrentWord());
+  }
+
+  shuffleLetters(word) {
+    return word.split('').sort(() => Math.random() - 0.5).join('');
+  }
+
+  checkAnswer(input) {
+    const correctWord = this.getCurrentWord();
+    const isCorrect = input.toLowerCase() === correctWord.toLowerCase();
+    return {
+      correct: isCorrect,
+      word: correctWord,
+      definition: this.definitions[correctWord] || "Définition non disponible"
+    };
+  }
+
+  nextWord() {
+    this.currentIndex = (this.currentIndex + 1) % this.words.length;
+  }
+
+  reset() {
+    this.currentIndex = 0;
+    this.shuffleWords();
+  }
+}
 
